5aaf934762a9a59be769293174b20887d1d734e5
“dihtyar” <bogdandihtyar@gmail.com>
2017-10-14 22:15:36 +0300

    Create base structure

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
+
 var $ = require('jquery');
 var Str = require('./constant');
 
-var App = function(){
-    console.log(Str);
+var App = function() {
+    // console.log(Str);
     $('body').html(Str);
 };
 
 var app = new App();
+
+app.get = function() {};
