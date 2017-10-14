114634c068582b2f1687a91e90a1c7c92392707e
“dihtyar” <bogdandihtyar@gmail.com>
2017-10-14 20:25:36 +0300

    read file

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
+var $ = require('jquery');
+var Str = require('./constant');
+
 var App = function(){
-    console.log('App: init');
+    console.log(Str);
+    $('body').html(Str);
 };
 
 var app = new App();
